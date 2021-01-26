1e929037e4c6a6cb51b118fc770a9710410bf17c
DMMCE <dmmce2018@gmail.com>
2021-01-26 17:32:12 +0200

    Machine click fix

---
+++ Assets/MachineCollider.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MachineCollider : MonoBehaviour
+{
+    [SerializeField]
+    Transform ObjectToMove;
+    private Vector3 screenPoint;
+    private Vector3 offset;
+    [SerializeField]
+    MachineMovment GetMachineMovment;
+    float X_Limit;
+  
+    public void Move (Vector3 screenpnt, Vector3 ofset) {
+
+
+        screenPoint = screenpnt;
+        offset = ofset;
+
+    }
+  
+    void OnMouseDrag()
+    {
+        if (GetMachineMovment.canMove)
+        {
+            Vector3 curScreenPoint = new Vector3(Input.mousePosition.x , Input.mousePosition.y , screenPoint.z);
+
+            Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
+            float x = Mathf.Clamp(curPosition.x , -GetMachineMovment.Get_XLimit() , GetMachineMovment.Get_XLimit());
+            ObjectToMove.localPosition = new Vector3(x , 0f , 0f);
+        }
+    }
+}
--- Assets/Scripts/Machine/MachineMovment.cs
+++ Assets/Scripts/Machine/MachineMovment.cs
@@ -8,36 +8,40 @@ public class MachineMovment : MonoBehaviour
     [SerializeField] Collider2D GetCollider;
     private Vector3 screenPoint;
     private Vector3 offset;
+    [SerializeField]
+    MachineCollider GetMachineCollider;
+    public bool canMove =false;
 
-    bool canMove =false;
     private void Update()
     {
         if (!BallsInSceneManager.isThereAnyBallInScene()) { canMove = false; GetCollider.enabled = false; }
         else { GetCollider.enabled = true; canMove = true; }
+
+
+        if (canMove)
+        {
+          RaycastHit2D raycastHit2D= Physics2D.Raycast(new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x ,
+                Camera.main.ScreenToWorldPoint(Input.mousePosition).y) , Vector2.zero , 0f);
+            if (raycastHit2D)
+            if (raycastHit2D.collider.CompareTag("MachineCollider"))
+            {
+                screenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.localPosition);
+
+                offset = gameObject.transform.localPosition - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x , Input.mousePosition.y , screenPoint.z));
+
+                    GetMachineCollider.Move(screenPoint,offset);
+            }
+        }
     }
     public void Set_XLimit(float value)
     {
         X_Limit = value;
     }
-    void OnMouseDown()
+    public float Get_XLimit(   )
     {
-        if (canMove)
-        {
-            screenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.localPosition);
-
-            offset = gameObject.transform.localPosition - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x , Input.mousePosition.y , screenPoint.z));
-        }
+      return  X_Limit  ;
     }
 
-    void OnMouseDrag()
-    {
-        if (canMove)
-        {
-            Vector3 curScreenPoint = new Vector3(Input.mousePosition.x , Input.mousePosition.y , screenPoint.z);
 
-            Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
-            float x = Mathf.Clamp(curPosition.x , -X_Limit , X_Limit);
-            transform.localPosition = new Vector3(x , 0f , 0f);
-        }
-    }
+    
 }
